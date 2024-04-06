f4271aaa9ebdc7784c83d5613ed6617390d0928a
ymoumin <ymoum044@uottawa.ca>
2024-04-06 11:20:49 -0400

    login access

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
+import {AuthenticationService} from "../services/authentication.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
+  imports: [
+    NgIf
+  ],
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  authenticated = false;
 
   switchToFrench(): void {
     // Add functionality to switch the language to French here
     console.log("Switching to French");
   }
 
+  constructor(private authService: AuthenticationService) {
+  }
+
+  ngOnInit(){
+    this.authService.currentUser$.subscribe(user => {
+      if(user){
+        this.authenticated = true;
+      }
+    })
+
+  }
 }
